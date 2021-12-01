53dde1de97e0f07e9e69b3693b84d44e05a37bb2
wffirilat <wffirilat@users.noreply.github.com>
2021-11-30 19:50:19 -0600

    Cleanup heightmapIndependentSpawning algorithm

--- src/main/java/carpetsurplus/mixins/SpawnHelperMixin.java
+++ src/main/java/carpetsurplus/mixins/SpawnHelperMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.SpawnHelper;
+import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -25,11 +26,11 @@ public class SpawnHelperMixin {
     private static void spawnEntitiesInChunk(SpawnGroup group, ServerWorld world, WorldChunk chunk, SpawnHelper.Checker checker, SpawnHelper.Runner runner, CallbackInfo ci) {
         if (CarpetSurplusSettings.heightmapIndependentSpawning) {
             ChunkPos chunkPos = chunk.getPos();
-            int maxY = chunk.getHighestNonEmptySectionYOffset() + 16;
-            for (int secY = world.getBottomY(); secY < maxY; secY += 16) {
-                if(world.random.nextFloat() > 0.25f) continue;
+            for (ChunkSection section : chunk.getSectionArray()) {
+                if (section.isEmpty()) continue;
+                if (world.random.nextFloat() > 0.25f) continue;
                 int x = chunkPos.getStartX() + world.random.nextInt(16);
-                int y = secY + world.random.nextInt(16);
+                int y = section.getYOffset() + world.random.nextInt(16);
                 int z = chunkPos.getStartZ() + world.random.nextInt(16);
                 SpawnHelper.spawnEntitiesInChunk(group, world, chunk, new BlockPos(x, y, z), checker, runner);
             }
